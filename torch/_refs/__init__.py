[WIP] fix softmax decomposition handling of zero dimensions
```python
import torch
import torch.nn.functional as F

t = torch.rand((4, 0, 0))
print("~")
print(F.softmax(t, dim=-1))  # this passes
print("~")
torch._refs.softmax(t, dim=0)  # this fails
print("~")
```

ghstack-source-id: 9dd4c629ae0bf6de14195b14e3d34cf29b2ee351
Pull Request resolved: https://github.com/pytorch/pytorch/pull/91322

torch/_refs/__init__.py
@@ -712,7 +712,9 @@ def logsumexp(
     # ATen specifies int[1] type dims which expands integers to tuples of length 1
     if not isinstance(dim, Iterable):
         dim = (dim,)
-    if utils.is_float_dtype(a.dtype) or utils.is_complex_dtype(a.dtype):
+    if (
+        utils.is_float_dtype(a.dtype) or utils.is_complex_dtype(a.dtype)
+    ) and a.numel() > 0:
         # For float and complex dtypes, we shift input to exp by a constant to avoid overflow
         a_max = amax(a, dim, keepdim=True)
         a_max = where(abs(a_max) == float("inf"), 0.0, a_max)
@@ -3361,6 +3363,8 @@ def softmax(
     dtype: Optional[torch.dtype] = None,
 ) -> TensorLikeType:
     result_dtype = dtype or a.dtype
+    if a.numel() == 0:
+        return torch.zeros_like(a, dtype=result_dtype)
     computation_dtype = utils.get_computation_dtype(result_dtype)
     a_ = _maybe_convert_to_dtype(a, computation_dtype)
     a_max = amax(a_, dim, keepdim=True)